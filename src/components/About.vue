7dae4d1ed46fd9348a44c551fbd38e81dd1ba8fb
Coralie-V <71003152+Coralie-V@users.noreply.github.com>
2021-03-10 00:24:03 +0100

    Page à propos

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,6 +1,35 @@
 <template>
   <div class="container">
-    <h1>{{ msg }}</h1>
+    <div class="a-propos row my-5">
+      <div class="texte col-12 col-md-7">
+        <h2>Présentation</h2>
+        <p>
+          Je m'appelle Coralie, j'ai 19 ans, je suis actuellement en 2ème année de DUT MMI (Métiers du Multimédia et de l'Internet).
+        </p>
+        <p>
+          J'ai rejoint cette formation car
+        </p>
+        <p>
+          Je suis passionnée par l'audiovisuel, les animes et les mangas.
+        </p>
+      </div>
+      <div class="photo col-12 col-md-5">
+        <img class="image-moi" src="static/img/photo-coralie.jpg" alt="photo de moi">
+      </div>
+      <div class="texte competences col-12 col-md-7">
+        <h2>Compétences</h2>
+        <p>HTML/CSS/JS</p>
+        <p>VueJs</p>
+        <p>Bootstrap</p>
+        <p>WordPress</p>
+        <p>Adobe Premiere</p>
+        <p>Adobe Photoshop</p>
+        <p>Adobe Illustrator</p>
+        <p>Adobe XD</p>
+        <p>Adobe After Effect</p>
+      </div>
+      <a class="cv" href="http://coralievarrier.fr/coralie-varrier-cv.pdf">Accéder au CV</a>
+    </div>
 
   </div>
 </template>
@@ -20,5 +49,46 @@ export default {
 <style scoped>
 h1, h2 {
   font-weight: normal;
+}
+.container .projet p{
+  margin: 1.5rem 0;
+}
+hr{
+  color: #dc3545;
+  background-color: #dc3545;
+
+}
+.image-moi{
+  margin: 2rem 0;
+  max-width: 400px;
+}
+
+.a-propos{
+  margin-bottom: 10rem!important;
+}
+p{
+  font-family: "Noto Sans", sans-serif;
+}
+.a-propos h2{
+  font-weight: bold;
+}
+
+@media (max-width: 576px) {
+  .image-moi{
+    max-width: 300px;
+  }
+}
+.competences p{
+  display: inline-block;
+  border: black solid 1px;
+  border-radius: 80px;
+  padding: 0.30rem;
+  margin: 0.2rem;
+}
+.cv{
+  margin-top: 3.5rem;
+  margin-left: 1rem;
+  color: #dc3545;
+
 }
 </style>
